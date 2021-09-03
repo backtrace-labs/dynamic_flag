071f9db83ec64186e5ddf07181ca1f46419912eb
Austin Hicks <ahicks@backtrace.io>
2021-09-03 09:57:06 -0700

    anhook: add a test program
    Summary:
    We can prove that it works.  I didn't bother with a CLI parser: unless things
    go badly wrong, we'll only ever use this once.
    
    Output:
    
    ```
    Before init
    off:printf1
    off:printf2
    on:printf1
    on:printf2
    on:printf3
    untouched:printf1
    untouched:printf2
    Initial:
    on:printf1
    on:printf2
    on:printf3
    untouched:printf2
    Activating off:printf1
    off:printf1
    on:printf1
    on:printf2
    on:printf3
    untouched:printf2
    Deactivating on:.*
    off:printf1
    untouched:printf2
    Activating on:printf3
    off:printf1
    on:printf3
    untouched:printf2
    ```
    
    Depends on Dxxxx
    Refs Txxxx
    
    Test Plan: Run it
    
    Reviewers: pkhuong, wandrews, sbahra
    
    Reviewed By: pkhuong
    
    Maniphest Tasks: Txxxx
    
    Differential Revision: https://foo.backtrace.io/Dxxxx

---
+++ tests/feature_flags.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+
+#include "an_hook.h"
+
+
+static void
+run_all(void)
+{
+
+	AN_HOOK(off, printf1) {
+		printf("off:printf1\n");
+	}
+
+	AN_HOOK(off, printf2) {
+		printf("off:printf2\n");
+	}
+
+	AN_HOOK_ON(on, printf1) {
+		printf("on:printf1\n");
+	}
+
+	AN_HOOK_ON(on, printf2) {
+		printf("on:printf2\n");
+	}
+
+	AN_HOOK_ON(on, printf3) {
+		printf("on:printf3\n");
+	}
+
+	AN_HOOK(untouched, printf1) {
+		printf("untouched:printf1\n");
+	}
+
+	AN_HOOK_ON(untouched, printf2) {
+		printf("untouched:printf2\n");
+	}
+
+	return;
+}
+
+static void
+wrapped_activate(const char *pat)
+{
+
+	printf("Activating %s\n", pat);
+	an_hook_activate(pat);
+	return;
+}
+
+static void
+wrapped_deactivate(const char *pat)
+{
+
+		printf("Deactivating %s\n", pat);
+		an_hook_deactivate(pat);
+		return;
+}
+
+int main(int argc, char **argv)
+{
+
+	printf("Before init\n");
+	run_all();
+	an_hook_init_lib();
+
+	printf("Initial:\n");
+	run_all();
+
+	wrapped_activate("off:printf1");
+	run_all();
+
+	wrapped_deactivate("on:.*");
+	run_all();
+
+	wrapped_activate("on:printf3");
+	run_all();
+
+	return 0;
+}
