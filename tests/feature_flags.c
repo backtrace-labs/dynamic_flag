c760bfbe9224e678662c2a70fe23665e4eba31a7
Paul Khuong <pkhuong@backtrace.io>
2021-10-25 13:44:43 -0400

    an_hook/tests: explain what output we expect from feature_flags
    Ref Txxxx.

--- tests/feature_flags.c
+++ tests/feature_flags.c
@@ -85,57 +85,195 @@ main(int argc, char **argv)
 	(void)argv;
 
 	printf("Before init\n");
+	/*
+	 * Expected:
+	 * Before init
+	 * off:printf1
+	 * off:printf2
+	 * on:printf1
+	 * on:printf2
+	 * on:printf3
+	 * test:on:printf3
+	 * untouched:printf1
+	 * untouched:printf2
+	 * feature_flag:default_on
+	 */
 	run_all();
 	dynamic_flag_init_lib();
 
 	printf("\nList all flags\n");
-	dynamic_flag_list_state(".*", dynamic_flag_list_fprintf_cb, NULL);
+	/*
+	 * Expected:
+	 * List all flags
+	 * feature_flag:default_off@tests/feature_flags.c:55 (off): DF_FEATURE flags are classic feature flags: off initially and if the dynamic_flag machine can't find them, and the compiler expects them to be disabled
+	 * feature_flag:default_on@tests/feature_flags.c:48 (on)
+	 * none:dummy@src/dynamic_flag.c:158 (off): This dummy flag does nothing. It lets the dynamic_flag library compile even when no other flag is defined.
+	 * off:printf1@tests/feature_flags.c:12 (off): DF_OPT flags are usually disabled, but should always be safe to enable
+	 * off:printf2@tests/feature_flags.c:16 (off)
+	 * on:printf1@tests/feature_flags.c:21 (on): DF_DEFAULT flags are enabled initially and when the library can't find them.
+	 * on:printf2@tests/feature_flags.c:27 (on): DF_DEFAULT_SLOW flags are enabled like DF_DEFAULT, but instruct the compiler to expect them to be disabled.
+	 * on:printf3@tests/feature_flags.c:31 (on)
+	 * test:on:printf3@tests/feature_flags.c:36 (off)
+	 * untouched:printf1@tests/feature_flags.c:40 (off)
+	 * untouched:printf2@tests/feature_flags.c:44 (on)
+	 */
+	dynamic_flag_list_state(".*", dynamic_flag_list_fprintf_cb, stdout);
 
 	printf("\nInitial:\n");
+	/*
+	 * Expected:
+	 * Initial:
+	 * on:printf1
+	 * on:printf2
+	 * on:printf3
+	 * untouched:printf2
+	 * feature_flag:default_on
+	 */
 	run_all();
 
 	wrapped_activate("off:printf1");
+	/*
+	 * Expected:
+	 * Activating off:printf1
+	 * off:printf1
+	 * on:printf1
+	 * on:printf2
+	 * on:printf3
+	 * untouched:printf2
+	 * feature_flag:default_on
+	 */
 	run_all();
 
 	wrapped_activate("^test:on:printf3");
+	/*
+	 * Expected:
+	 * Activating ^test:on:printf3
+	 * off:printf1
+	 * on:printf1
+	 * on:printf2
+	 * on:printf3
+	 * test:on:printf3
+	 * untouched:printf2
+	 * feature_flag:default_on
+	 */
 	run_all();
 
 	wrapped_deactivate(".*on:.*");
+	/*
+	 * Expected:
+	 * Deactivating .*on:.*
+	 * off:printf1
+	 * untouched:printf2
+	 * feature_flag:default_on
+	 */
 	run_all();
 
 	wrapped_activate("on:printf3");
+	/*
+	 * Expected:
+	 * Activating on:printf3
+	 * off:printf1
+	 * on:printf3
+	 * untouched:printf2
+	 * feature_flag:default_on
+	 */
 	run_all();
 
 	wrapped_deactivate("feature_flag:.*");
+	/*
+	 * Expected:
+	 * Deactivating feature_flag:.*
+	 * off:printf1
+	 * on:printf3
+	 * untouched:printf2
+	 */
 	run_all();
 
 	wrapped_activate("feature_flag:default_off");
+	/*
+	 * Expected:
+	 * Activating feature_flag:default_off
+	 * off:printf1
+	 * on:printf3
+	 * untouched:printf2
+	 * feature_flag:default_off
+	 */
 	run_all();
 
 	printf("\nActivating feature_flag\n");
 	dynamic_flag_activate_kind(feature_flag, ".*");
+	/*
+	 * Expected:
+	 * Activating feature_flag
+	 * off:printf1
+	 * on:printf3
+	 * untouched:printf2
+	 * feature_flag:default_on
+	 * feature_flag:default_off
+	 */
 	run_all();
 
 	printf("\nDeactivating feature_flag\n");
 	dynamic_flag_deactivate_kind(feature_flag, ".*");
+	/* 
+	 * Expected:
+	 * Deactivating feature_flag
+	 * off:printf1
+	 * on:printf3
+	 * untouched:printf2
+	 * feature_flag:default_off
+	 */
 	run_all();
 
 	printf("\nUnhooking feature_flag:.*");
 	dynamic_flag_unhook("feature_flag:.*");
 	wrapped_activate("feature_flag:.*");
+	/*
+	 * Expected:
+	 * Unhooking feature_flag:.*
+	 * Activating feature_flag:.*
+	 * off:printf1
+	 * on:printf3
+	 * untouched:printf2
+	 * feature_flag:default_off
+	 */
 	run_all();
 
 	printf("\nDeactivating feature_flag:.*\n");
 	dynamic_flag_deactivate_kind(feature_flag, NULL);
+	/*
+	 * Expected:
+	 * Deactivating feature_flag:.*
+	 * off:printf1
+	 * on:printf3
+	 * untouched:printf2
+	 */
 	run_all();
 
 	printf("\nRehooking feature_flag:.*");
 	dynamic_flag_rehook("feature_flag:.*");
 	wrapped_activate("feature_flag:.*");
+	/*
+	 * Expected:
+	 * Rehooking feature_flag:.*
+	 * Activating feature_flag:.*
+	 * off:printf1
+	 * on:printf3
+	 * untouched:printf2
+	 * feature_flag:default_on
+	 * feature_flag:default_off
+	 */
 	run_all();
 
 	printf("\nDeactivating feature_flag\n");
 	dynamic_flag_deactivate_kind(feature_flag, NULL);
+	/*
+	 * Expected:
+	 * Deactivating feature_flag
+	 * off:printf1
+	 * on:printf3
+	 * untouched:printf2
+	 */
 	run_all();
 
 	return 0;
